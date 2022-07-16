decad7079790f892bf171cf720f5687ec8f9b208
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-07-16 15:32:17 +0100

    pop

---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,22 @@
+import { MainComponent } from "./main.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+    children: [{ path: "", redirectTo: "profile", pathMatch: "full" }],
+  },
+  {
+    path: "",
+    redirectTo: "home/profile",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MainRoutingModule {}
---
+++ src/app/main/main.module.ts
@@ -0,0 +1,154 @@
+import { MainRoutingModule } from "./main-routing.module";
+import { MainComponent } from "./main.component";
+import { TreeTableModule } from "primeng/treetable";
+import { ChipsModule } from "primeng/chips";
+import { ChipModule } from "primeng/chip";
+import { CarouselModule } from "primeng/carousel";
+import { StepsModule } from "primeng/steps";
+import { NgModule } from "@angular/core";
+import {
+  CommonModule,
+  CurrencyPipe,
+  DatePipe,
+  NgSwitch,
+} from "@angular/common";
+
+import { ProgressBarModule } from "primeng/progressbar";
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+import { TabViewModule } from "primeng/tabview";
+import { InputTextModule } from "primeng/inputtext";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DropdownModule } from "primeng/dropdown";
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { SpinnerModule } from "primeng/spinner";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { DialogModule } from "primeng/dialog";
+import { LoadingComponent } from "../components/loading/loading.component";
+import { TooltipModule } from "primeng/tooltip";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import { InputSwitchModule } from "primeng/inputswitch";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { FileUploadModule } from "primeng/fileupload";
+import { CardModule } from "primeng/card";
+import { MultiSelectModule } from "primeng/multiselect";
+import { TreeModule } from "primeng/tree";
+import { KeyFilterModule } from "primeng/keyfilter";
+import { ContactComponent } from "../components/contact/contact.component";
+import { PanelModule } from "primeng/panel";
+import { ChartModule } from "primeng/chart";
+import {
+  DialogService,
+  DynamicDialogConfig,
+  DynamicDialogRef,
+} from "primeng/dynamicdialog";
+import { FieldsetModule } from "primeng/fieldset";
+import { CalendarModule } from "primeng/calendar";
+import { PickListModule } from "primeng/picklist";
+import { TabMenuModule } from "primeng/tabmenu";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { RatingModule } from "primeng/rating";
+import { InputNumberModule } from "primeng/inputnumber";
+import { DragDropModule } from "primeng/dragdrop";
+import { HumanizePipe } from "../custom-pipes/humanize";
+import { TimeoutComponent } from "../components/timeout/timeout.component";
+import { CheckboxModule } from "primeng/checkbox";
+import { SliderModule } from "primeng/slider";
+import { SplitterModule } from "primeng/splitter";
+import { DataViewModule } from "primeng/dataview";
+import { MessageModule } from "primeng/message";
+import { InputMaskModule } from "primeng/inputmask";
+import { ConfirmationService } from "primeng/api";
+import { NodeService } from "../demo/service/nodeservice";
+import { ProductService } from "../demo/service/productservice";
+import { AccordionModule } from "primeng/accordion";
+import { AvatarModule } from "primeng/avatar";
+import { AvatarGroupModule } from "primeng/avatargroup";
+import { EventService } from "../demo/service/eventservice";
+import { ToggleButtonModule } from "primeng/togglebutton";
+import { KnobModule } from "primeng/knob";
+import { FullCalendarModule } from "primeng/fullcalendar";
+import { ToolbarModule } from "primeng/toolbar";
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    LoadingComponent,
+    HumanizePipe,
+    TimeoutComponent,
+  ],
+  imports: [
+    AvatarModule,
+    AvatarGroupModule,
+    CommonModule,
+    MainRoutingModule,
+    ProgressBarModule,
+    ButtonModule,
+    InputNumberModule,
+    ToolbarModule,
+    InputMaskModule,
+    KnobModule,
+    RippleModule,
+    TabViewModule,
+    ToggleButtonModule,
+    AccordionModule,
+    CalendarModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    FormsModule,
+    TableModule,
+    ToastModule,
+    SpinnerModule,
+    ProgressSpinnerModule,
+    DialogModule,
+    TooltipModule,
+    ConfirmDialogModule,
+    InputSwitchModule,
+    RadioButtonModule,
+    FileUploadModule,
+    CardModule,
+    MultiSelectModule,
+    TreeModule,
+    KeyFilterModule,
+    PanelModule,
+    ChartModule,
+    FieldsetModule,
+    CalendarModule,
+    PickListModule,
+    TabMenuModule,
+    InputTextareaModule,
+    AutoCompleteModule,
+    RatingModule,
+    DragDropModule,
+    StepsModule,
+    CheckboxModule,
+    SliderModule,
+    SplitterModule,
+    DataViewModule,
+    InputSwitchModule,
+    CarouselModule,
+    ChipsModule,
+    ChipModule,
+    TreeTableModule,
+    MessageModule,
+    FullCalendarModule,
+  ],
+  exports: [],
+  providers: [
+    DialogService,
+    NgSwitch,
+    DynamicDialogConfig,
+    DynamicDialogRef,
+    ConfirmationService,
+    NodeService,
+    ProductService,
+    CurrencyPipe,
+    DatePipe,
+    EventService,
+  ],
+  entryComponents: [TimeoutComponent],
+})
+export class MainModule {}
