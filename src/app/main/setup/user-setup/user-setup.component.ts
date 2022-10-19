89fdc7979065c2fb8c84a0105e8c61db71c34e68
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-10-19 17:53:53 +0100

    POP

--- src/app/main/setup/user-setup/user-setup.component.ts
+++ src/app/main/setup/user-setup/user-setup.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { BreadcrumbService } from "src/app/breadcrumb.service";
 
 @Component({
-  selector: 'app-user-setup',
-  templateUrl: './user-setup.component.html',
-  styleUrls: ['./user-setup.component.scss']
+  selector: "app-user-setup",
+  templateUrl: "./user-setup.component.html",
+  styleUrls: ["./user-setup.component.scss"],
 })
 export class UserSetupComponent implements OnInit {
+  @ViewChild("formWrapper") public formWrapper: ElementRef;
+  userForm: FormGroup;
+  allRoles: any[];
+  theRole: any;
+  editingUser: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private breadcrumbService: BreadcrumbService,
+    public confirmationService: ConfirmationService,
+    public messageService: MessageService
+  ) {
+    this.userForm = fb.group({
+      LastName: ["", Validators.required],
+      FirstName: ["", Validators.required],
+      OtherName: [""],
+      CodeName: [""],
+      Mobile: ["", Validators.required],
+      Email: [""],
+      Address: [""],
+      DefaultPassword: ["", Validators.required],
+      AssignedRole: ["", Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+    this.breadcrumbService.setItems([
+      {
+        label: "Setup",
+        routerLink: ["/main/setup"],
+      },
+      {
+        label: "User",
+        routerLink: ["/main/setup/user"],
+      },
+    ]);
+  }
+
+  CreateUser() {}
+
+  UpdateUser() {}
 }
