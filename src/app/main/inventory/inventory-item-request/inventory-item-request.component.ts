45c34b81b5f5242fbe76a5354b2bce910cb94308
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-11-10 22:49:59 +0100

    Push

--- src/app/main/inventory/inventory-item-request/inventory-item-request.component.ts
+++ src/app/main/inventory/inventory-item-request/inventory-item-request.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MessageService, ConfirmationService } from "primeng/api";
+import { BreadcrumbService } from "src/app/breadcrumb.service";
 
 @Component({
-  selector: 'app-inventory-item-request',
-  templateUrl: './inventory-item-request.component.html',
-  styleUrls: ['./inventory-item-request.component.scss']
+  selector: "app-inventory-item-request",
+  templateUrl: "./inventory-item-request.component.html",
+  styleUrls: ["./inventory-item-request.component.scss"],
 })
 export class InventoryItemRequestComponent implements OnInit {
+  @ViewChild("formWrapper") public formWrapper: ElementRef;
+  requestForm: FormGroup;
+  allInventoryItems: any[];
+  theInventoryItem: any;
+  selectedInventoryRawItems: any[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    fb: FormBuilder,
+    public messageService: MessageService,
+    private breadcrumbService: BreadcrumbService,
+    public confirmationService: ConfirmationService
+  ) {
+    this.requestForm = fb.group({
+      Name: ["", Validators.required],
+      Description: ["", Validators.required],
+      Quantity: ["", Validators.required],
+      Item: ["", Validators.required],
+      BasePrice: [""],
+    });
   }
 
+  ngOnInit(): void {
+    this.breadcrumbService.setItems([
+      {
+        label: "Inventory",
+        routerLink: ["/main/inventory"],
+      },
+      {
+        label: "Item Request",
+        routerLink: ["/main/inventory/item-request"],
+      },
+    ]);
+  }
+
+  // Check User Reuuesyt
+  //INCLUDE INVENTORY CONFIRMATION IN
+  CreateInventoryRequest() {}
 }
