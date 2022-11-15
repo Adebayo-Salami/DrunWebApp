a63ee07966fac41f95b99f9fc8d2c394c791d9df
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-11-15 17:21:37 +0100

    adad

--- src/app/interfaces/inventory-operation.ts
+++ src/app/interfaces/inventory-operation.ts
@@ -31,3 +31,14 @@ export interface ItemRequestRawMaterialVM {
   itemId: number;
   quantity: number;
 }
+
+export interface UpdateInventoryItemRequestVM {
+  requestName?: string;
+  requestDescription?: string;
+  requestedItemId?: number;
+  requestedPackSizeId?: number;
+  requestedQuantity?: number;
+  unitPrice?: number;
+  productSupplierId?: number;
+  rawMaterials?: ItemRequestRawMaterialVM[];
+}
