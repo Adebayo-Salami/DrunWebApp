97a45bd7ad40f1415bf6a7066e9263776dcbd468
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-11-16 21:15:49 +0100

    asa

--- src/app/main/inventory/inventory-item-approval/inventory-item-approval.component.ts
+++ src/app/main/inventory/inventory-item-approval/inventory-item-approval.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { MessageService, ConfirmationService } from "primeng/api";
 import { BreadcrumbService } from "src/app/breadcrumb.service";
 import { InventoryItem } from "src/app/interfaces/inventory-item";
 import {
+  ActOnInventoryItemRequestVM,
   InventoryItemRequest,
   InventoryItemRequestRawMaterial,
 } from "src/app/interfaces/inventory-operation";
@@ -23,6 +24,7 @@ import { UserService } from "src/app/services/user.service";
 })
 export class InventoryItemApprovalComponent implements OnInit {
   @ViewChild("formWrapper") public formWrapper: ElementRef;
+  cautionForm: FormGroup;
   allPendingRequests: InventoryItemRequest[];
   selectedPendingRequests: InventoryItemRequest[];
   fetchingPendingRequests: boolean;
@@ -34,6 +36,9 @@ export class InventoryItemApprovalComponent implements OnInit {
   allUsers: User[];
   itemRequestInView: InventoryItemRequest;
   allStoreItems: InventoryStoreItem[];
+  openCautionDialogue: boolean;
+  cautionText: string;
+  cautionCode: number;
 
   constructor(
     fb: FormBuilder,
@@ -45,7 +50,9 @@ export class InventoryItemApprovalComponent implements OnInit {
     public messageService: MessageService,
     private breadcrumbService: BreadcrumbService,
     public confirmationService: ConfirmationService
-  ) {}
+  ) {
+    this.cautionForm = fb.group({});
+  }
 
   ngOnInit(): void {
     this.breadcrumbService.setItems([
@@ -238,9 +245,19 @@ export class InventoryItemApprovalComponent implements OnInit {
     this.itemRequestInView = item;
   }
 
-  ShowDeclineItemRequested() {}
+  ShowDeclineItemRequested() {
+    this.cautionText =
+      "Are you sure you want to decline this item request. This is an irreversible action. Do you still wish to proceed?";
+    this.cautionCode = 1;
+    this.openCautionDialogue = true;
+  }
 
-  ShowApproveItemRequested() {}
+  ShowApproveItemRequested() {
+    this.cautionText =
+      "Are you sure you want to approve this item request. This is an irreversible action. Do you still wish to proceed?";
+    this.cautionCode = 1;
+    this.openCautionDialogue = true;
+  }
 
   GetItemName(itemId: number): string {
     let item = this.allItems.find((x) => x.id == itemId);
@@ -270,5 +287,66 @@ export class InventoryItemApprovalComponent implements OnInit {
     if (storeItem) return storeItem.quantityInStore;
 
     return 0;
+  }
+
+  CautionAction() {
+    this.messageService.add({
+      severity: "info",
+      summary: "Notice",
+      detail: "Declining Item Request...",
+    });
+
+    const postData: ActOnInventoryItemRequestVM = {
+      itemRequestId: this.itemRequestInView.id,
+      isApproved: this.cautionCode == 1 ? true : false,
+    };
+
+    this.inventoryOperationService
+      .ActOnInventoryItemRequest(postData)
+      .subscribe(
+        async (data) => {
+          if (!data.isSuccessful) {
+            this.messageService.add({
+              severity: "error",
+              summary: "Failure",
+              detail: data.message,
+            });
+            console.log("Error: " + JSON.stringify(data));
+            return;
+          }
+
+          this.messageService.add({
+            severity: "success",
+            summary: "Completed",
+            detail: "Action Successfull...",
+          });
+
+          this.fetchingPendingRequests = true;
+          this.HideCautionDialog();
+          const index = this.allPendingRequests.indexOf(this.itemRequestInView);
+          if (index > -1) {
+            this.allPendingRequests.splice(index, 1);
+          }
+          this.itemRequestInView = null;
+          this.fetchingPendingRequests = false;
+        },
+        (error) => {
+          console.log("Error: " + JSON.stringify(error));
+          this.messageService.add({
+            severity: "error",
+            summary: "Notice",
+            detail:
+              "Unable to act on item request at the moment.. Reason: [" +
+              error.message +
+              "]",
+          });
+        }
+      );
+  }
+
+  HideCautionDialog() {
+    this.openCautionDialogue = false;
+    this.cautionCode = null;
+    this.cautionText = null;
   }
 }
