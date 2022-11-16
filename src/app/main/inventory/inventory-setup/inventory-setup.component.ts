bdb81bdf9a78a987ecc9b028c0e213c6a5227309
Adebayo Salami <salamibolarinwa16@gmail.com>
2022-11-16 13:48:31 +0100

    Push

--- src/app/main/inventory/inventory-setup/inventory-setup.component.ts
+++ src/app/main/inventory/inventory-setup/inventory-setup.component.ts
@@ -1,11 +1,17 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 import { ConfirmationService, MessageService } from "primeng/api";
 import { BreadcrumbService } from "src/app/breadcrumb.service";
 import { InventoryApprovingOfficer } from "src/app/interfaces/inventory-approving-officer";
 import {
   CreateInventoryItemVM,
   InventoryItem,
+  UpdateInventoryItemVM,
 } from "src/app/interfaces/inventory-item";
 import { PackSize } from "src/app/interfaces/pack-size";
 import { User } from "src/app/interfaces/user";
@@ -36,6 +42,7 @@ export class InventorySetupComponent implements OnInit {
   theUser: User;
   fetchingApprovingOfficers: boolean;
   allApprovingOfficers: InventoryApprovingOfficer[];
+  itemToEdit: InventoryItem;
 
   constructor(
     private fb: FormBuilder,
@@ -171,13 +178,134 @@ export class InventorySetupComponent implements OnInit {
     );
   }
 
-  CloseEditingItem() {}
+  CloseEditingItem() {
+    this.editingItem = false;
+    this.itemToEdit = null;
+    this.isRawMaterialRadioButton = null;
+    this.itemForm.reset();
+  }
 
-  UpdateItem() {}
+  UpdateItem() {
+    this.messageService.add({
+      severity: "info",
+      summary: "Notice",
+      detail: "Updating Inventory Item...",
+    });
 
-  EditItem(item: any) {}
+    const id = this.itemToEdit.id;
+    const postData: UpdateInventoryItemVM = {
+      name: this.itemForm.get("Name").value,
+      description: this.itemForm.get("Description").value,
+      isRawMaterial: this.isRawMaterialRadioButton == 1 ? true : false,
+    };
 
-  DeleteItem(item: any) {}
+    this.inventoryItemService.UpdateInventoryItem(id, postData).subscribe(
+      async (data) => {
+        if (!data.isSuccessful) {
+          this.messageService.add({
+            severity: "error",
+            summary: "Failure",
+            detail: data.message,
+          });
+          console.log("Error: " + JSON.stringify(data));
+          return;
+        }
+
+        this.messageService.add({
+          severity: "success",
+          summary: "Notice",
+          detail: "Update Successful!",
+        });
+        this.CloseEditingItem();
+        this.FetchAllItems();
+      },
+      (error) => {
+        console.log("Error: " + JSON.stringify(error));
+        this.messageService.add({
+          severity: "error",
+          summary: "Notice",
+          detail:
+            "Unable to update inventory item at the moment.. Reason: [" +
+            error.message +
+            "]",
+        });
+      }
+    );
+  }
+
+  EditItem(item: InventoryItem) {
+    this.editingItem = true;
+    this.itemForm.addControl(
+      "ID",
+      new FormControl({ value: "", disabled: true }, Validators.required)
+    );
+
+    this.itemToEdit = item;
+    this.itemForm.patchValue({
+      ID: item.id,
+      Name: item.name,
+      Description: item.description,
+    });
+    this.isRawMaterialRadioButton = item.itemType;
+
+    this.formWrapper.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "start",
+    });
+  }
+
+  DeleteItem(item: InventoryItem) {
+    this.confirmationService.confirm({
+      message: "Are you sure you want to remove item?",
+      accept: () => {
+        this.messageService.add({
+          severity: "info",
+          summary: "Notice",
+          detail: "Removing item...",
+        });
+
+        this.inventoryItemService.DeleteInventoryItem(item.id).subscribe(
+          async (data) => {
+            if (!data.isSuccessful) {
+              this.messageService.add({
+                severity: "error",
+                summary: "Failure",
+                detail: data.message,
+              });
+              console.log("Error: " + JSON.stringify(data));
+              return;
+            }
+
+            this.messageService.add({
+              severity: "success",
+              summary: "Removed",
+              detail: "Removed successfully",
+            });
+
+            this.fetchingItems = true;
+            this.CloseEditingItem();
+            const index = this.allItems.indexOf(item);
+            if (index > -1) {
+              this.allItems.splice(index, 1);
+            }
+            this.fetchingItems = false;
+          },
+          (error) => {
+            console.log("Error: " + JSON.stringify(error));
+            this.messageService.add({
+              severity: "error",
+              summary: "Notice",
+              detail:
+                "Unable to remove item at the moment.. Reason: [" +
+                error.message +
+                "]",
+            });
+          }
+        );
+      },
+    });
+  }
 
   CreatePackSize() {}
 
@@ -185,11 +313,11 @@ export class InventorySetupComponent implements OnInit {
 
   UpdatePackSize() {}
 
-  EditPackSize(item: any) {}
+  EditPackSize(item: PackSize) {}
 
-  DeletePackSize(item: any) {}
+  DeletePackSize(item: PackSize) {}
 
   CreateApprovingOfficer() {}
 
-  DeleteApprovingOfficer(item: any) {}
+  DeleteApprovingOfficer(item: InventoryApprovingOfficer) {}
 }
