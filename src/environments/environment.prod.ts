aaa30c5d0b5de14d9aac24aa9ce213ca180591d4
Ebunoluwa B <157045233+123biobaku@users.noreply.github.com>
2025-05-30 00:40:46 +0100

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   Production: true,
-  BaseUrl: "https://drunbackend.herokuapp.com/api/",
-  CompanyId: 1
+  // BaseUrl: "https://drunbackend.herokuapp.com/api/",
+  BaseUrl: "https://drunappbackend.somee.com/api/",
+  CompanyId: 1,
 };
